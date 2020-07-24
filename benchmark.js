96b0fb186beb9466c812b6d6ae74c0afb735544c
그늘로 <entrydark@gmail.com>
2020-07-24 10:46:50 +0900

    Update benchmark.js

--- benchmark.js
+++ benchmark.js
@@ -12,7 +12,11 @@ suite.add("SVD", () => {
 .add("NIPALS", () => {
     const pca = new PCA(dataset, {method: "NIPALS"})
 })
+.add("NIPALS, {nCompNIPALS: 5}", () => {
+    const pca = new PCA(dataset, {method: "NIPALS", nCompNIPALS: 5})
+})
 .on("complete", function(){
     console.log('Fastest is ' + this.filter('fastest').map('name'))
+    console.log(this[0].hz, this[1].hz, this[2].hz, this[3].hz)
 })
 .run()
